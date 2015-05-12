bb8d42c4feacda3b08be5e58902742dd37919951
MartinHartmannJensen <mhartdk@hotmail.com>
2015-05-12 12:00:44 +0200

    Ændringer til navbaren, virker ikke som tiltænkt endnu

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -7,8 +7,30 @@ using System.Web.UI.WebControls;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+
+    List<System.Web.UI.HtmlControls.HtmlGenericControl> controllist;
+
+    System.Web.UI.HtmlControls.HtmlGenericControl activeselector = null;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        //controllist = new List<System.Web.UI.HtmlControls.HtmlGenericControl>();
+        //controllist.Add(navhomelink);
+        //controllist.Add(navaboutlink);
+        //controllist.Add(navcontactlink);
+        //navaboutlink.Attributes.Add("class", "active");
+        if (activeselector == null)
+            activeselector = navhomelink;
+        activeselector.Attributes.Add("class", "active");
+    }
 
+    protected void navhomelink_click(object sender, EventArgs e)
+    {
+        activeselector = navhomelink;
+    }
+
+    protected void navaboutlink_click(object sender, EventArgs e)
+    {
+        activeselector = navaboutlink;
     }
 }
