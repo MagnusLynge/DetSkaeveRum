b66df9c81af847567b3f42da29a8e927ee9c819b
kevinjensen4 <1026967@ucn.dk>
2015-05-12 11:26:23 +0200

    Tilføjet Database connection
    Tilføjet Database connection og lavet en klasse som kan kaldes når man
    ønsker at integrer med databasen som er static så der kun findes en
    instance af connectionen

---
+++ App_Code/DBCon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Activities.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class DBCon
+{
+
+    private static DBDataContext _db = new DBDataContext();
+    private static DBCon _instance;
+
+	private DBCon()
+	{
+		
+	}
+
+    public static DBCon Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new DBCon();
+            }
+            return _instance;
+        }
+        
+    }
+
+    public static DBDataContext GetDB()
+    {
+        return _db;
+    }
+
+
+}
