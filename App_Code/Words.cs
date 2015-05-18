f8dbb377a894ae1c865b78ef8a8a911632292beb
kevinjensen4 <1026967@ucn.dk>
2015-05-18 09:35:28 +0200

    added stuff to word
    added stuff to word

--- App_Code/Words.cs
+++ App_Code/Words.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-
-
-
 public interface IWords
 {
-
-    
-
+    List<Word> GetAll();
+    List<Word> GetWord(string word);
+    void CreateWord(string word);
 
 }
 
 
 /// <summary>
-/// Summary description for Words
+/// This is a class for words
 /// </summary>
 public class Words
 {
-	public Words()
+    private DBDataContext _db = DBCon.GetDB();
+
+	public Words(int id, string word)
 	{
-		//
-		// TODO: Add constructor logic here
-		//
+	    Id = id;
+	    Word = word;
 	}
+
+    public void CreateWord(string word)
+    {
+        var query = new Word { Word1 = word};
+        _db.Words.InsertOnSubmit(query);
+        _db.SubmitChanges();
+    }
+
+    public List<Word> GetAllWord()
+    {
+        var query = _db.Words.Select(i => i);
+        return query.ToList();
+    }
+
+    public List<Word> GetWord(string word)
+    {
+        var query = _db.Words.Where(i => i.Word1 == word).Select(i => i);
+        return query.ToList();
+    }
+
+
+
+
+    public int Id { get; set; }
+    public string Word { get; set; }
+
 }
