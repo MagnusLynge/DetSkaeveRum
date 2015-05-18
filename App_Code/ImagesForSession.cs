dbac6964d275e764c35ac410cd17bc13c5362d58
Magnus Lynge Johansen <MagnusLynge1990@hotmail.com>
2015-05-18 13:20:48 +0200

    Images : ID
    Tilføj id til images

--- App_Code/ImagesForSession.cs
+++ App_Code/ImagesForSession.cs
@@ -16,10 +16,25 @@ public class ImagesForSession
 		//
 	}
 
+    public void DeleteImage(int id)
+    {
+        Image delImg = _db.Images.Where(i => i.id == id).Select(i => i).FirstOrDefault();
+        if (delImg == null) return;
+        _db.Images.DeleteOnSubmit(delImg);
+        _db.SubmitChanges();
+    }
+
     public void AddImage(string fileName) 
     {
         var query = new Image { FileName = fileName };
         _db.Images.InsertOnSubmit(query);
         _db.SubmitChanges();
+    }
+
+    public int GetImageId(string fileName)
+    {
+        var query = _db.Images.Where(i => i.FileName == fileName).Select(i => i).First();
+
+        return query.id;
     }
 }
