f35ca9c1fd273ff32d2ee895436389c4b3a3637e
kevinjensen4 <1026967@ucn.dk>
2015-05-18 10:38:06 +0200

    commit det her

--- WordPage.aspx.cs
+++ WordPage.aspx.cs
@@ -7,14 +7,14 @@ using System.Web.UI.WebControls;
 
 public partial class WordPage : System.Web.UI.Page
 {
+    WordForSession wrd = new WordForSession();
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        WordGrid.DataSource = word.getAllWords();
+        WordGrid.DataSource = wrd.GetAllWords();
         WordGrid.DataBind();
     }
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
-        var newWord = txtWord.Text;
-        word.InsertWord(newWord);
     }
 }
