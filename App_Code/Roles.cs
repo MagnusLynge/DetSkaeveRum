5eb1c055da3e2f0879114e26910461ff2a0807f3
MartinHartmannJensen <mhartdk@hotmail.com>
2015-05-18 11:04:36 +0200

    Roles update

--- App_Code/Roles.cs
+++ App_Code/Roles.cs
@@ -3,17 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-public interface IRoles
-{
-    List<Roles> GetAllRoles();
-    List<Roles> GetRole(string role);
-    void CreateRole(string role);
-    void UpdateRole(string role);
-    void DeleteRole(string role);
-
-}
-
-public class Roles : IRoles
+public class Roles
 {
     private DBDataContext _db = DBCon.GetDB();
 
@@ -43,13 +33,13 @@ public class Roles : IRoles
         _db.SubmitChanges();
     }
 
-    public List<Roles> GetAllRoles()
+    public List<Role> GetAllRoles()
     {
         var query = _db.Roles.Select(i => i);
         return query.ToList();
     }
 
-    public List<Roles> GetRole(string role)
+    public List<Role> GetRole(string role)
     {
         var query = _db.Roles.Where(i => i.Role1 == role).Select(i => i);
         return query.ToList();
