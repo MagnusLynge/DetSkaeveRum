81e782363c8314080ced9436853a64eea47055dd
MartinHartmannJensen <mhartdk@hotmail.com>
2015-05-20 08:51:21 +0200

    Navbar buttons - tilføjet class="active" ved pageLoad

--- About.aspx.cs
+++ About.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
 
 public partial class About : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        var pageBtn = (HtmlGenericControl)Master.FindControl("navaboutlink");
+        pageBtn.Attributes.Add("class", "active");
     }
 }
