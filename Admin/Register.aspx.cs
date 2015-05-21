7324b4bc326d8739a8158472c37c3185ab7663d8
JunderDehn <hirraan51@msn.com>
2015-05-21 11:11:57 +0200

    Login

--- Admin/Register.aspx.cs
+++ Admin/Register.aspx.cs
@@ -6,6 +6,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+
 
 public partial class Admin_Register : System.Web.UI.Page
 {
@@ -19,13 +22,18 @@ public partial class Admin_Register : System.Web.UI.Page
         // Default UserStore constructor uses the default connection string named: DefaultConnection
         var userStore = new UserStore<IdentityUser>();
         var manager = new UserManager<IdentityUser>(userStore);
-
         var user = new IdentityUser() { UserName = UserName.Text };
+
         IdentityResult result = manager.Create(user, Password.Text);
 
         if (result.Succeeded)
         {
             StatusMessage.Text = string.Format("User {0} was created successfully!", user.UserName);
+            //var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            //var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+            //authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+            //Response.Redirect("~/Login.aspx");
+
         }
         else
         {
--- LoginPage.aspx.cs
+++ LoginPage.aspx.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
 public partial class LoginPage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("~/Homepage.aspx");
+            }
+        }
+    }
 
+    protected void SignIn(object sender, EventArgs e)
+    {
+        var userStore = new UserStore<IdentityUser>();
+        var userManager = new UserManager<IdentityUser>(userStore);
+        var user = userManager.Find(UserName.Text, Password.Text);
+
+        if (user != null)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+            authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, userIdentity);
+            Response.Redirect("~/Homepage.aspx");
+        }
+        else
+        {
+            //StatusText.Text = "Invalid username or password.";
+            lblStatus.Visible = true;
+        }
     }
 }
