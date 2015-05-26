32451da255d99aa866231e98a6f21c4b365df4ea
kevinjensen4 <1026967@ucn.dk>
2015-05-26 10:27:08 +0200

    tabels

--- CreateSession.aspx.cs
+++ CreateSession.aspx.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
 
 public partial class CreateSession : System.Web.UI.Page
 {
     ImagesForSession img = new ImagesForSession();
     WordForSession wrd = new WordForSession();
     RolesForSession rol = new RolesForSession();
+    CreateSes newSes = new CreateSes();
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -21,6 +24,15 @@ public partial class CreateSession : System.Web.UI.Page
 
         repRolesOnSes.DataSource = rol.GetAllRoles();
         repRolesOnSes.DataBind();
+    }
+
+    protected void btnCreateSession_Click(object sender, EventArgs e)
+    {
+        var userID = User.Identity.GetUserId();
+        newSes.CreateNewSession(Convert.ToInt32(userID), true);
+
+
+        
     }
 
 
